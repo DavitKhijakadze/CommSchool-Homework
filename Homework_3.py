dad73c16be21a012ba63c76812756f2715780b26
DavitKhijakadze <dato.khijakadze0@gmail.com>
2026-05-24 11:39:01 +0400

    task3 done

--- Homework_3.py
+++ Homework_3.py
@@ -1,32 +1,47 @@
-#task1
-
-while True:
-
-    char = input("Please enter any char: ")
-
-    if len(char) > 1:
-        print("Enter 1 letter: ")   
-        print(15 * "=")
-
-    elif not char.isalpha():
-        print("This is not alphabet, try again: ")
-        print(15 * "=")
-
-    elif char.lower() in "aeiou":
-        print(f"{char} is a vowel")
-        print(15 * "=")
-        break
-
-    else:
-        print(f"{char} is a consonant")
-        print(15 * "=")
-        break
+import random
 
 
-#task2
+# #task1
 
-for i in range(10,0,-1):
-    print(i, end=" ")
+# while True:
+
+#     char = input("Please enter any char: ")
+
+#     if len(char) > 1:
+#         print("Enter 1 letter: ")   
+#         print(15 * "=")
+
+#     elif not char.isalpha():
+#         print("This is not alphabet, try again: ")
+#         print(15 * "=")
+
+#     elif char.lower() in "aeiou":
+#         print(f"{char} is a vowel")
+#         print(15 * "=")
+#         break
+
+#     else:
+#         print(f"{char} is a consonant")
+#         print(15 * "=")
+#         break
+
+
+# #task2
+
+# for i in range(10,0,-1):
+#     print(i, end=" ")
 
 
 #task3
+
+lst = [random.randint(1, 20) for _ in range(10)]
+indexed_lst = list(enumerate(lst))
+sorted_index = sorted(indexed_lst, key=lambda x: x[1], reverse=True)
+
+for original_index, value in sorted_index[:3]:
+    print(f"data {value} - index {original_index}")
+
+print(f"Main list: {lst}")
+
+
+#task4
