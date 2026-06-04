6563fd86faa4cc0123a8dfa63eff3ec33eca8063
DavitKhijakadze <dato.khijakadze0@gmail.com>
2026-06-04 22:43:44 +0400

    task 7-10 done

--- Homework_5.py
+++ Homework_5.py
@@ -65,3 +65,96 @@ for length in range(1, 4):
 
 output_result = ", ".join(all_combinations)
 print(output_result)
+
+
+#task7
+
+start_time = time.time()
+random_number = random.randint(1,20)
+
+while True:
+    user_input = input("guess the number: ")
+    end_time = time.time()
+    duration = round(end_time - start_time,1)
+    
+    if duration > 5:
+        print("you lose")
+        print(f"time duration: {duration}")
+        break
+        
+    if user_input == random_number and duration <= 5:
+        print(f"it's correct. comp number{random_number} - your number{user_input}")
+        print(f"time duration: {duration}")
+        break
+    else:
+        print("try again")
+        continue
+        
+        
+#task8
+start = datetime.now()
+player1 = start + timedelta(seconds=random.randint(5,20))
+player2 = start + timedelta(seconds=random.randint(5,20))
+player1_time = player1 - start
+player2_time = player2 - start
+
+if player1_time > player2_time:
+    print(f"player2 won! player1 time {player1_time} - player2 time {player2_time}")
+elif player1_time < player2_time:
+    print(f"player1 won! player1 time {player1_time} - player2 time {player2_time}")
+else:
+    print("its draw")
+
+
+
+#task9
+
+today = date.today()
+birthday = date(2000, 12, 10)
+next_birthday = date(today.year, birthday.month, birthday.day)
+
+if next_birthday < today:
+    next_birthday = date(today.year + 1, birthday.month, birthday.day)
+
+days_left = (next_birthday - today).days
+
+print(days_left)
+
+
+#task10
+
+secret_password = tuple(random.choices(range(1, 7), k=4))
+all_combinations = itertools.product(range(1, 7), repeat=4)
+counter = 0
+
+for guess in all_combinations:
+    counter +=1
+    print(f"try {counter}: {guess}")
+     
+    if guess == secret_password:
+        print(f"password match: {guess}")
+        break
+
+"""
+
+git checkout main & git pull origin main
+
+git checkout -b exercises-1-6
+
+git push origin exercises-1-6
+
+git checkout main
+
+git checkout -b exercises-7-10
+
+git push origin exercises-7-10
+
+git checkout main
+
+git merge exercises-1-6
+
+git merge exercises-7-10
+
+git push origin main
+
+"""
