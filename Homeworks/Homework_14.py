7a33ba9d3242fe8896b6a0fa9b9ffe1ea3f08d9a
DavitKhijakadze <dato.khijakadze0@gmail.com>
2026-07-22 20:55:36 +0400

    done

--- Homeworks/Homework_14.py
+++ Homeworks/Homework_14.py
@@ -3,125 +3,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 import time
+import unittest
 
 
-# #task1
 
-# driver = webdriver.Chrome()
-# driver.get("https://www.saucedemo.com/")
+#task1
 
-
-# username = driver.find_element(By.ID, "user-name")
-# password = driver.find_element(By.ID, "password")
-# login_button = driver.find_element(By.ID, "login-button")
-
-# username.send_keys("locked_out_user")
-# password.send_keys("secret_sauce")
-
-# login_button.click()
-
-# time.sleep(2)
-
-# try:
-#     error = driver.find_element(By.CSS_SELECTOR, "[data-test='error']")
-#     print("ავტორიზაცია ვერ გაიარა. შეცდომის შეტყობინება:")
-#     print(error.text)
-# except:
-#     print("ავტორიზაციამ წარმატებით გაიარა ან ერორის ბლოკი ვერ მოიძებნა.")
-
-# driver.quit()
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
 
 
-# #task2
+username = driver.find_element(By.ID, "user-name")
+password = driver.find_element(By.ID, "password")
+login_button = driver.find_element(By.ID, "login-button")
 
-# options = Options()
-# options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
+username.send_keys("locked_out_user")
+password.send_keys("secret_sauce")
 
-# driver = webdriver.Chrome(options=options)
-# driver.get("https://www.saucedemo.com/")
+login_button.click()
 
+time.sleep(2)
 
-# username = driver.find_element(By.ID, "user-name")
-# password = driver.find_element(By.ID, "password")
-# login_button = driver.find_element(By.ID, "login-button")
+try:
+    error = driver.find_element(By.CSS_SELECTOR, "[data-test='error']")
+    print("ავტორიზაცია ვერ გაიარა. შეცდომის შეტყობინება:")
+    print(error.text)
+except:
+    print("ავტორიზაციამ წარმატებით გაიარა ან ერორის ბლოკი ვერ მოიძებნა.")
 
-# username.send_keys("performance_glitch_user")
-# password.send_keys("secret_sauce")
-
-# login_button.click()
-
-# time.sleep(2)
-
-# burger_menu = driver.find_element(By.ID, "react-burger-menu-btn")
-# burger_menu.click()
-
-# time.sleep(2)
-
-# logout_btn = driver.find_element(By.ID, "logout_sidebar_link")
-# logout_btn.click()
-
-# time.sleep(2)
-
-# driver.quit()
+driver.quit()
 
 
-# #task3
-
-# options = Options()
-# options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
-
-# driver = webdriver.Chrome(options=options)
-# driver.get("https://www.saucedemo.com/")
-
-
-# username = driver.find_element(By.ID, "user-name")
-# password = driver.find_element(By.ID, "password")
-# login_button = driver.find_element(By.ID, "login-button")
-
-# username.send_keys("problem_user")
-# password.send_keys("secret_sauce")
-
-# login_button.click()
-
-# time.sleep(2)
-
-# item1 = driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
-# item2 = driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light")
-
-# item1.click()
-# item2.click()
-
-# time.sleep(2)
-
-# cart_btn = driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-link']")
-# cart_btn.click()
-
-# time.sleep(2)
-
-# remove_item1 = driver.find_element(By.ID, "remove-sauce-labs-backpack")
-# remove_item2 = driver.find_element(By.ID, "remove-sauce-labs-bike-light")
-
-# time.sleep(2)
-
-# remove_item1.click()
-# remove_item2.click()
-
-# time.sleep(2)
-
-# burger_menu = driver.find_element(By.ID, "react-burger-menu-btn")
-# burger_menu.click()
-
-# time.sleep(2)
-
-# logout_btn = driver.find_element(By.ID, "logout_sidebar_link")
-# logout_btn.click()
-
-# time.sleep(2)
-
-# driver.quit()
-
-
-#task4
+#task2
 
 options = Options()
 options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
@@ -134,7 +47,40 @@ username = driver.find_element(By.ID, "user-name")
 password = driver.find_element(By.ID, "password")
 login_button = driver.find_element(By.ID, "login-button")
 
-username.send_keys("standard_user")
+username.send_keys("performance_glitch_user")
+password.send_keys("secret_sauce")
+
+login_button.click()
+
+time.sleep(2)
+
+burger_menu = driver.find_element(By.ID, "react-burger-menu-btn")
+burger_menu.click()
+
+time.sleep(2)
+
+logout_btn = driver.find_element(By.ID, "logout_sidebar_link")
+logout_btn.click()
+
+time.sleep(2)
+
+driver.quit()
+
+
+#task3
+
+options = Options()
+options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.saucedemo.com/")
+
+
+username = driver.find_element(By.ID, "user-name")
+password = driver.find_element(By.ID, "password")
+login_button = driver.find_element(By.ID, "login-button")
+
+username.send_keys("problem_user")
 password.send_keys("secret_sauce")
 
 login_button.click()
@@ -152,66 +98,134 @@ time.sleep(2)
 cart_btn = driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-link']")
 cart_btn.click()
 
-time.sleep(5)
+time.sleep(2)
 
 remove_item1 = driver.find_element(By.ID, "remove-sauce-labs-backpack")
+remove_item2 = driver.find_element(By.ID, "remove-sauce-labs-bike-light")
+
+time.sleep(2)
+
 remove_item1.click()
+remove_item2.click()
 
 time.sleep(2)
-
-back_btn = driver.find_element(By.ID, "continue-shopping")
-back_btn.click()
-
-time.sleep(2)
-
-find_item = driver.find_element(By.ID, "item_1_title_link")
-find_item.click()
-
-time.sleep(5)
-
-back_btn1 = driver.find_element(By.ID, "back-to-products")
-back_btn1.click()
-
-time.sleep(2)
-
-sort_element = driver.find_element(By.CSS_SELECTOR, "[data-test='product-sort-container']")
-select = Select(sort_element)
-select.select_by_value("hilo")
-
-time.sleep(2)
-
-main_window = driver.current_window_handle
-
-facebook_icon = driver.find_element(By.CSS_SELECTOR, "[data-test='social-facebook']")
-facebook_icon.click()
-time.sleep(2)
-
-for window_handle in driver.window_handles:
-    if window_handle != main_window:
-        driver.switch_to.window(window_handle)
-        driver.close()
-        break
-
-driver.switch_to.window(main_window)
-
-linkedin_icon = driver.find_element(By.CSS_SELECTOR, "[data-test='social-linkedin']")
-linkedin_icon.click()
-time.sleep(2)
-
-for window_handle in driver.window_handles:
-    if window_handle != main_window:
-        driver.switch_to.window(window_handle)
-        driver.close()
-        break
-
-driver.switch_to.window(main_window)
 
 burger_menu = driver.find_element(By.ID, "react-burger-menu-btn")
 burger_menu.click()
-time.sleep(1)
 
-logout_btn = driver.find_element(By.CSS_SELECTOR, "[data-test='logout-sidebar-link']")
+time.sleep(2)
+
+logout_btn = driver.find_element(By.ID, "logout_sidebar_link")
 logout_btn.click()
+
 time.sleep(2)
 
 driver.quit()
+
+
+#task4
+
+class SauceTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        options = Options()
+        options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
+
+        cls.driver = webdriver.Chrome(options=options)
+        cls.driver.implicitly_wait(10)
+        cls.driver.get("https://www.saucedemo.com/")
+        cls.main_window = cls.driver.current_window_handle
+
+
+    def test_1_login(self):
+        try:
+            username = self.driver.find_element(By.ID, "user-name")
+            password = self.driver.find_element(By.ID, "password")
+            login_button = self.driver.find_element(By.ID, "login-button")
+
+            username.send_keys("standard_user")
+            password.send_keys("secret_sauce")
+
+            login_button.click()
+
+            time.sleep(2)
+
+            self.assertIn("inventory.html", self.driver.current_url)
+            print("ავტორიზაციამ წარმატებით გაიარა!")
+
+        except Exception as e:
+            self.fail(f"ავტორიზაციის დროს დაფიქსირდა შეცდომა: {e}")
+
+
+    def test_2_cart_and_navigation(self):
+        item1 = self.driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
+        item2 = self.driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light")
+        item1.click()
+        item2.click()
+        time.sleep(2)
+
+        cart_btn = self.driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-link']")
+        cart_btn.click()
+        time.sleep(5)
+
+        remove_item1 = self.driver.find_element(By.ID, "remove-sauce-labs-backpack")
+        remove_item1.click()
+        time.sleep(2)
+
+        back_btn = self.driver.find_element(By.ID, "continue-shopping")
+        back_btn.click()
+        time.sleep(2)
+
+        find_item = self.driver.find_element(By.ID, "item_1_title_link")
+        find_item.click()
+        time.sleep(5)
+
+        back_btn1 = self.driver.find_element(By.ID, "back-to-products")
+        back_btn1.click()
+        time.sleep(2)
+
+
+    def test_3_sort_products(self):
+        sort_element = self.driver.find_element(By.CSS_SELECTOR, "[data-test='product-sort-container']")
+        select = Select(sort_element)
+        select.select_by_value("hilo")
+        time.sleep(2)
+
+
+    def test_4_footer_and_logout(self):
+        facebook_icon = self.driver.find_element(By.CSS_SELECTOR, "[data-test='social-facebook']")
+        facebook_icon.click()
+        time.sleep(2)
+        self._close_new_tab_and_return()
+
+        linkedin_icon = self.driver.find_element(
+            By.CSS_SELECTOR, "[data-test='social-linkedin']"
+        )
+        linkedin_icon.click()
+        time.sleep(2)
+        self._close_new_tab_and_return()
+
+        burger_menu = self.driver.find_element(By.ID, "react-burger-menu-btn")
+        burger_menu.click()
+        time.sleep(1)
+
+        logout_btn = self.driver.find_element(By.CSS_SELECTOR, "[data-test='logout-sidebar-link']")
+        logout_btn.click()
+        time.sleep(2)
+
+
+    def _close_new_tab_and_return(self):
+        for window_handle in self.driver.window_handles:
+            if window_handle != self.main_window:
+                self.driver.switch_to.window(window_handle)
+                self.driver.close()
+                break
+        self.driver.switch_to.window(self.main_window)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
